895980ff007b6447d1b0463f58fc0dbb8ecf6128
breno.mcosta <breno.mcosta@pbh.gov.br>
2024-01-18 15:09:18 -0300

    up

---
+++ src/main/java/com/order/OrderKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderKafkaServiceApplication.class, args);
+	}
+
+}
